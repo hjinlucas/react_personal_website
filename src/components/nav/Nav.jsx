bdca5aeb74325381363f3fdf4ee08ac494f80499
lucasjin <hjin.lucas@gmail.com>
2023-03-18 20:39:48 -0400

    Update navbar icons and assign ids for sections

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './nav.css'
+import{AiOutlineHome, AiOutlineUser, AiOutlineBook, AiOutlineMessage} from 'react-icons/ai'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+      <a href="# "><AiOutlineHome/></a>
+      <a href="#about"><AiOutlineUser/></a>
+      <a href="#experience"><AiOutlineBook/></a>
+      <a href="#contact"><AiOutlineMessage/></a>
+    </div>
   )
 }
 
