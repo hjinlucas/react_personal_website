b747db1d41a09a7063ef0d2937aa681bf00bd408
lucasjin <hjin.lucas@gmail.com>
2023-03-19 21:53:39 -0400

    Update Header Contents

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import HJ_photo from '../../assets/me_artsy_img_nobg.png'
 import HeaderContact from './HeaderContact'
 
 const Header = () => {
@@ -10,13 +9,9 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Hao Jin</h1>
-        <h5 className='text-light'>Fullstack Devloper test</h5>
+        <h5 className='text-light'>Fullstack Devloper</h5>
         <CTA />
         <HeaderContact />
-
-        <div className="HJ_photo">
-          <img src={HJ_photo} alt="" />
-        </div>
 
         <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
