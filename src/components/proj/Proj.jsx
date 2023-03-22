724ed689c8b0dea7e76c964c4e941de7780b4f03
lucasjin <hjin.lucas@gmail.com>
2023-03-22 00:25:14 -0400

    Update project section layout

--- src/components/proj/Proj.jsx
+++ src/components/proj/Proj.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import './proj.css';
+import img_project1 from '../../assets/img_project1.png';
+import img_project2 from '../../assets/img_project2.png';
+import img_project3 from '../../assets/img_project3.png';
+import { AiFillGithub } from 'react-icons/ai';
+
+
 
 const Proj = () => {
   return (
@@ -12,34 +18,38 @@ const Proj = () => {
           <div className='cards-wrapper'>
           <div className='card'>
             <div className='visual'>
-              <img src='https://via.placeholder.com/240x240' alt='' />
+              <img src={img_project1} alt="Project 1" />
             </div>
             <div className='text-content'>
               <h3>Project Title</h3>
               <p>Description of the project goes here.</p>
-              <a href='# '>View Project</a>
+              <a href="https://github.com/hjinlucas/FeedYourself" className="github-icon">
+                <AiFillGithub />
+              </a>
             </div>
           </div>
 
           <div className='card'>
             <div className='visual'>
-              <img src='https://via.placeholder.com/240x240' alt='' />
+              <img src={img_project2} alt='Project 2' />
             </div>
             <div className='text-content'>
               <h3>Project Title</h3>
               <p>Description of the project goes here.</p>
-              <a href='# '>View Project</a>
+              <a href="https://github.com/hjinlucas/react_personal_website" className="github-icon">
+                <AiFillGithub />
+              </a>
             </div>
           </div>
 
           <div className='card'>
             <div className='visual'>
-              <img src='https://via.placeholder.com/240x240' alt='' />
+              <img src={img_project3} alt='Project 3' />
             </div>
             <div className='text-content'>
               <h3>Project Title</h3>
               <p>Description of the project goes here.</p>
-              <a href='# '>View Project</a>
+              
             </div>
           </div>
           </div>
