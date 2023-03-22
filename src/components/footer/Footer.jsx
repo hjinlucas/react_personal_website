48cdade7d7da49df551a6a048d63cfac8789c235
lucasjin <hjin.lucas@gmail.com>
2023-03-22 00:35:15 -0400

    Create footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import './footer.css'
+import React from 'react';
+import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer id="footer">
+      <div className="container">
+        <p>
+          Developed by{' '}
+          <a
+            href="https://github.com/hjinlucas"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hao Jin (Lucas)
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
