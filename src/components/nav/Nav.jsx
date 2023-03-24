f37ffc6ae737af762ca33863bec5eeb2082d6b58
lucasjin <hjin.lucas@gmail.com>
2023-03-24 10:15:00 -0400

    Update nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './nav.css'
-import {AiOutlineHome, AiOutlineUser, AiOutlineBook, AiOutlineMessage} from 'react-icons/ai'
+import {AiOutlineHome, AiOutlineUser, AiOutlineBook, AiFillCode} from 'react-icons/ai'
 import { useState } from 'react'
 
 const Nav = () => {
@@ -12,7 +12,7 @@ const Nav = () => {
       <a href="# " onClick={() => setActiveNav('# ')} className={activeNav === '# ' ? 'active' : ''}><AiOutlineHome/></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><AiOutlineBook/></a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><AiOutlineMessage/></a>
+      <a href="#proj" onClick={() => setActiveNav('#proj')} className={activeNav === '#proj' ? 'active' : ''}><AiFillCode/></a>
     </nav>
   )
 }
